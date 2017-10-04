f0ddea454aaef435da1acb02259006674f8a153a
Subin <subin@apache.org>
2017-10-04 17:11:30 -0400

    add deployment

--- README.md
+++ README.md
@@ -11,26 +11,26 @@ docker build -t submod/inception-app -f Dockerfile .
 
 1. Ensure that you are connected to an OpenShift project
 ```
-	oc new-project test
+oc new-project test
 ```
 2. Create the template for inception web application.
 ```
-    oc create -f https://raw.githubusercontent.com/sub-mod/inception-app/master/template.json
+oc create -f https://raw.githubusercontent.com/sub-mod/inception-app/master/template.json
 ```
 3. Create the template for tensorflow serving endpoints.
 ```
-    oc create -f https://raw.githubusercontent.com/sub-mod/mnist-models/master/template.json
+oc create -f https://raw.githubusercontent.com/sub-mod/mnist-models/master/template.json
 ```
 4. Launch tensorflow serving s2i build for the tensorflow models.We need to create a prediction 
    endpoints with inception model.
 ```
-	oc new-app --template=tensorflow-server --param=APPLICATION_NAME=tf-inception \
-		--param=MODEL_IMAGE=submod/inception-model \
-		--param=IMAGE_MODEL_PATH=/models/inception/1/  \
-		--param=MODEL_NAME=inception \
-		--param=MODEL_PATH=1 
+oc new-app --template=tensorflow-server --param=APPLICATION_NAME=tf-inception \
+	--param=MODEL_IMAGE=submod/inception-model \
+	--param=IMAGE_MODEL_PATH=/models/inception/1/  \
+	--param=MODEL_NAME=inception \
+	--param=MODEL_PATH=1
 ```
 5. Launch the inception web application.
 ```
-	oc new-app --template=mnistapp --param=APPLICATION_NAME=inception-app --param=PREDICTION_SERVICE1=tf-inception 
+oc new-app --template=inceptionapp --param=APPLICATION_NAME=inception-app --param=PREDICTION_SERVICE1=tf-inception 
 ```
--- main.py
+++ main.py
@@ -22,25 +22,32 @@ app = Flask(__name__)
 def recognize():
     host = os.environ.get('PREDICTION_HOST1', '0.0.0.0')
     port = os.environ.get('PREDICTION_PORT1', '6006')
-    channel = implementations.insecure_channel(host, int(port))
-    stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
-      # Send request
-    f = request.files['image']
-    data = f.read()
-    tfrequest = predict_pb2.PredictRequest()
-    tfrequest.model_spec.name = 'inception'
-    tfrequest.model_spec.signature_name = 'predict_images'
-    tfrequest.inputs['images'].CopyFrom(tf.contrib.util.make_tensor_proto(data, shape=[1]))
-    result = stub.Predict(tfrequest, 10.0)  # 10 secs timeout
-    print(result)
-    jsonresult = MessageToJson(result)
-    finalresult = json.loads(jsonresult)
-    classes = finalresult["outputs"]["classes"]['stringVal']
-    scores = finalresult["outputs"]["scores"]["floatVal"]
-    results=[]
-    for x, y in zip(classes,scores):
-        results.append({'label': x.decode('base64'), 'score': y})
-    return render_template('result.html', results=results)
+    print "...."
+    try:
+        channel = implementations.insecure_channel(host, int(port))
+        stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+          # Send request
+        f = request.files['image']
+        data = f.read()
+        tfrequest = predict_pb2.PredictRequest()
+        tfrequest.model_spec.name = 'inception'
+        tfrequest.model_spec.signature_name = 'predict_images'
+        tfrequest.inputs['images'].CopyFrom(tf.contrib.util.make_tensor_proto(data, shape=[1]))
+        result = stub.Predict(tfrequest, 10.0)  # 10 secs timeout
+        print(result)
+        jsonresult = MessageToJson(result)
+        finalresult = json.loads(jsonresult)
+        classes = finalresult["outputs"]["classes"]['stringVal']
+        scores = finalresult["outputs"]["scores"]["floatVal"]
+        results=[]
+        for x, y in zip(classes,scores):
+            results.append({'label': x.decode('base64'), 'score': y})
+        return render_template('result.html', results=results)
+    except:
+        results=[]
+        results.append({'label': 'error', 'score': '500'})
+        return render_template('result.html', results=results)
+    
 
 
 @app.route('/')
